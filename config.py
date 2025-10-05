29e851be0f909d4869fab99d2caaf5d48bae8717
sunpark333 <99256042+sunpark333@users.noreply.github.com>
2025-10-05 09:25:42 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Bot configuration settings
+TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
+API_ID = os.getenv('API_ID')
+API_HASH = os.getenv('API_HASH')
+PHONE_NUMBER = os.getenv('PHONE_NUMBER')
+TWITTER_VID_BOT = os.getenv('TWITTER_VID_BOT', 'twittervid_bot')
+YOUR_CHANNEL_ID = int(os.getenv('YOUR_CHANNEL_ID'))
+TIMEZONE = os.getenv('TIMEZONE', 'Asia/Kolkata')
